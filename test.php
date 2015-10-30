c5e195b1a4d8e55074d3bb83ca8be24599726ad3
wucheng.wu <w11w23c58@126.com>
2015-10-30 23:54:03 +0800

    add two new files

---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>hello world</title>
+</head>
+<body>
+<h1>hello world</h1>
+<?php echo 'Hello World'; ?>
+<h2>hello world</h2>
+</body>
+</html>
