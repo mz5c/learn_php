b78b6f89d833254c800e3b6a7d0069ea84dff415
wucheng.wu <w11w23c58@126.com>
2015-11-01 13:00:09 +0800

    变量作用域1

--- test.php
+++ test.php
@@ -7,6 +7,13 @@
 <body>
 <h1>hello world</h1>
 <?php echo 'Hello World'; ?>
+<?php
+$arr = array('a','b','c');
+foreach($arr as $var){
+	echo $var;
+}	
+echo $var;
+?>
 <h2>hello world</h2>
 </body>
 </html>
