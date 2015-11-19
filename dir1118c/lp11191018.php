3f33736f3af96d6bda88bf3f955e198c63163a3a
wucheng.wu <w11w23c58@126.com>
2015-11-19 10:45:58 +0800

    php study11191045

---
+++ dir1118c/lp11191018.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/11/19
+ * Time: 10:18
+ */
+echo <<<hello
+<form action="lp11181103.php">
+    name:<input type="text"><br>
+    age:<input type="text"><br>
+    <button type="submit">submit</button>
+    <button type="reset">reset</button>
+</form>
+hello;
