f6fce605e4abccca8f3c420f57be17c23cacd82f
wucheng.wu <w11w23c58@126.com>
2015-11-20 11:03:23 +0800

    lp11201102

---
+++ dir1118c/lp11201040.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>hello world</title>
+</head>
+<body>
+<h1>hello world</h1>
+<?php
+if(5=='5') {
+    ?>
+    <p style="color: green;">true</p>
+    <?php
+}else {
+    ?>
+    <p style="color: red;">false</p>
+    <?php
+}
+?>
+</body>
+</html>
