505d1af54e12f684838a730e7026b6342f30db39
wucheng.wu <w11w23c58@126.com>
2015-12-08 12:08:21 +0800

    fwrite

---
+++ dir1207c/lp12071218.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/12/7
+ * Time: 12:18
+ */
+$a = '..hello...';
+echo $a.'<br>';
+$b = trim($a,'.');
+echo $b;
+echo date("Y-m-d",strtotime("-1 day"));
+echo '<hr>';
+$handle = fopen('res.txt','a');
+fwrite($handle,'hello world ');
+fclose($handle);
+$str = file_get_contents('res.txt');
+echo $str;
