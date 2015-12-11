c51435c6b841109a296728034901a89425891083
wucheng.wu <w11w23c58@126.com>
2015-12-11 10:22:09 +0800

    test "" ''

---
+++ dir1207c/lp12111008.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/12/11
+ * Time: 10:08
+ */
+$title[1]="💥帮帮她！李沧某社区卫生中心给一岁半宝宝打错疫苗，家长维权难！";
+echo $title[1];echo '<hr>';
+$res = ltrim($title[1],"💥");
+echo $res;echo '<hr>';
+echo "💥";
