201751310f76f08bd1efc436ffba5f8c569eb808
wucheng.wu <w11w23c58@126.com>
2015-12-14 13:37:45 +0800

    fixed

--- dir1207c/lp12111008.php
+++ dir1207c/lp12111008.php
@@ -5,8 +5,11 @@
  * Date: 2015/12/11
  * Time: 10:08
  */
+$a = "🐨林森浩临刑前专访：死刑对我是一种偿债";
+$b = "🐨城会玩！男子冰面挖小洞&nbsp;秒钓大鱼震惊众人";
 $title[1]="💥帮帮她！李沧某社区卫生中心给一岁半宝宝打错疫苗，家长维权难！";
 echo $title[1];echo '<hr>';
-$res = ltrim($title[1],"💥");
+$res = ltrim(ltrim($title[1],"🐨"),"💥");
 echo $res;echo '<hr>';
 echo "💥";
+var_dump("🐨"==="💥");
