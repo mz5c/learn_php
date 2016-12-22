0d0310d831d91c7e5dd575639cca46e3b0a83857
wucheng <w11w23c58@126.com>
2016-12-22 10:59:43 +0800

    docs: delete

--- dir1118c/lp11201638.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type:image/png');
-$ch = curl_init("http://weixin.sogou.com/antispider/util/seccode.php?tc=1448000189");
-    //curl_setopt($ch,CURLOPT_COOKIEJAR, $cookieFile); // 把返回来的cookie信息保存在文件中
-    curl_exec($ch);
-    curl_close($ch);
